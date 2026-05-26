3376ab528507c770a55296c1970a396c75ab4475
Anton Savchenko <166227202+Rupert8@users.noreply.github.com>
2026-05-26 14:22:50 +0300

    Add: products-of-array-discluding-self - submission-0

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-0.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        List<Integer> multipliedNumbers = new ArrayList();
+
+        for(int i = 0; i < nums.length; i++) {
+            List<Integer> tempMultiplied = new ArrayList();
+            int temp = 1;
+            for(int j = 0; j < nums.length; j++) {
+                if(i != j) {
+                    temp = temp * nums[j];
+                }              
+            }
+            multipliedNumbers.add(temp);
+        }
+
+        int[] result = multipliedNumbers.stream()
+                                        .mapToInt(Integer::intValue)
+                                        .toArray();
+        return result;
+        }
+}  
